ab0ee5964cc36001444c3a566eeb2216d1442332
Shiva Kant Mishra <127529918+Shivakant18@users.noreply.github.com>
2026-08-21 02:05:52 +0530

    Time: 4 ms (100.00%) | Memory: 46.1 MB (33.42%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
+        if( x < 0 ){
             return false;
-                }
+        }
         int n = x;
         int rev = 0;
-        while(n > 0){
+        while( n > 0){
             int d = n % 10;
             rev = rev * 10 + d;
             n = n / 10;
@@ -13,9 +13,9 @@ class Solution {
         if( x == rev){
             return true;
         }
-      else{
-          return false;
+        else{
+            return false;
+        }
+        
     }
-    }
-
 }
