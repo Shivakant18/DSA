473bec3ed0ed5beda8a6d64396e77b606658f8e6
Shiva Kant Mishra <127529918+Shivakant18@users.noreply.github.com>
2026-08-21 20:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (34.21%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -7,15 +7,17 @@ class Solution(object):
         if not strs:
             return ""
 
-        # Pehli string ke har index aur character par loop
-        for i in range(len(strs[0])):
-            char = strs[0][i]
-            
-            # Baaki saari strings se compare karna
-            for string in strs[1:]:
-                # Agar string choti pad jaye YA character match na kare
-                if i == len(string) or string[i] != char:
-                    return strs[0][:i]
-                    
-        # Agar saare characters bina kisi mismatch ke pass ho gaye
-        return strs[0]
+        # 1. Array ko alphabetically sort karo
+        strs.sort()
+
+        first = strs[0]
+        last = strs[-1]
+        ans = []
+
+        # 2. Sirf pehli aur aakhri string ke characters compare karo
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                break
+            ans.append(first[i])
+
+        return "".join(ans)
