faa07b4d3ed59cb9f3c64182fd1f2b1299d94fb1
Shiva Kant Mishra <127529918+Shivakant18@users.noreply.github.com>
2026-09-14 13:00:45 +0530

    Time: 4 ms (89.72%) | Memory: 62.9 MB (84.32%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,15 +3,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0){
+    if (x < 0 || (x % 10 === 0 && x !==0)) {
         return false;
     }
-    let n = x;
-    let rev = 0;
 
-    while (x > 0){
-        rev = (rev * 10) + (x % 10);
-        x = Math.floor (x / 10);
+    let rev = 0;
+    while (x > rev) {
+        rev =  (rev * 10) + (x % 10)
+        x = (x / 10) | 0;
     }
-     return rev == n;
+
+    return x === rev || x === ((rev / 10) | 0);
+    
 };
